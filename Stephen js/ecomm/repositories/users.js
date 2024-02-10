5fbb9d5a5343e0eeea564bc101171ad4c2b7b19f
jydnbach <jaydenkim95@gmail.com>
2024-02-10 19:01:31 +0530

    ecomm login

--- Stephen js/ecomm/repositories/users.js
+++ Stephen js/ecomm/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
   constructor(filename) {
@@ -22,6 +23,8 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
+
     const records = await this.getAll();
     records.push(attrs);
 
@@ -35,18 +38,49 @@ class UsersRepository {
     );
   }
   randomId() {
-    return Math.random() * 9999999;
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((records) => records.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository("users.json");
-
-  await repo.create({ email: "test@test.com", password: "password" });
-
-  const users = await repo.getAll();
-
-  console.log(users);
-};
-
-test();
+module.exports = new UsersRepository("users.json");
