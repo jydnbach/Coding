da5092a15384422db805dc36ce7e041af5c3cba6
jydnbach <jaydenkim95@gmail.com>
2024-02-13 15:08:20 +0530

    testing

---
+++ Stephen js/hidash/index.test.js
@@ -0,0 +1,30 @@
+const assert = require("assert");
+const { forEach, map } = require("./index");
+
+// const test = (desc, fn) => {
+//   console.log("---", desc);
+//   try {
+//     fn();
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+it("the forEach function", () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum += value;
+  });
+
+  assert.strictEqual(sum, 6, "Expected forEAch to sum the array");
+});
+
+it("The map function", () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
+
+  assert.strictEqual(result[0], 2);
+  assert.strictEqual(result[1], 4);
+  assert.strictEqual(result[2], 6);
+});
