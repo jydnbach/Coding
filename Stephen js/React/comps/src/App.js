38ceadb5b11cbff2f52782be5e3e0a0a2e4b019c
jydnbach <jaydenkim95@gmail.com>
2024-02-17 19:17:24 +0530

    react button

--- Stephen js/React/comps/src/App.js
+++ Stephen js/React/comps/src/App.js
@@ -1,5 +1,42 @@
+import { GoBell, GoDatabase, GoCloudDownload } from "react-icons/go";
+import Button from "./components/Button";
+
 function App() {
-  return <div>App</div>;
+  const handleClick = () => {
+    console.log("click");
+  };
+  return (
+    <div>
+      <div>
+        <Button success rounded onClick={handleClick} className="mb-5">
+          <GoBell />
+          click
+        </Button>
+      </div>
+      <div>
+        <Button danger onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          But Now
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase />
+          See Deal
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline rounded>
+          Hide ads
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          something
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
