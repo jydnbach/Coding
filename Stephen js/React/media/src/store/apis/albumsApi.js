923f514fa25d063c5a88a82c960c69d0ec272725
jydnbach <jaydenkim95@gmail.com>
2024-02-26 17:13:21 +0530

    redux toolkit query

---
+++ Stephen js/React/media/src/store/apis/albumsApi.js
@@ -0,0 +1,70 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+//dev only
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+    fetchFn: async (...args) => {
+      //remove for production
+      await pause(1000);
+      return fetch(...args);
+    },
+  }),
+  endpoints(builder) {
+    return {
+      removeAlbum: builder.mutation({
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
+      addAlbum: builder.mutation({
+        invalidatesTags: (result, error, user) => {
+          return [{ type: "Album", id: user.id }];
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
+      fetchAlbums: builder.query({
+        providesTags: (results, error, user) => {
+          return [{ type: "Album", id: user.id }];
+        },
+
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
+export { albumsApi };
