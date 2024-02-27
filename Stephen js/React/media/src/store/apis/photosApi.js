2615a6d99bd1ba72ba076ebeacaf3c36c73e0b07
jydnbach <jaydenkim95@gmail.com>
2024-02-27 14:01:04 +0530

    photos final

--- Stephen js/React/media/src/store/apis/photosApi.js
+++ Stephen js/React/media/src/store/apis/photosApi.js
@@ -1,29 +1,39 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { faker } from '@faker-js/faker';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
 
 const photosApi = createApi({
-  reducerPath: 'photos',
+  reducerPath: "photos",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3005',
+    baseUrl: "http://localhost:3005",
   }),
   endpoints(builder) {
     return {
       fetchPhotos: builder.query({
+        providesTags: (result, error, album) => {
+          const tags = result.map((photo) => {
+            return { type: "Photo", id: photo.id };
+          });
+          tags.push({ type: "AlbumPhoto", id: album.id });
+          return tags;
+        },
         query: (album) => {
           return {
-            url: '/photos',
+            url: "/photos",
             params: {
               albumId: album.id,
             },
-            method: 'GET',
+            method: "GET",
           };
         },
       }),
       addPhoto: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "AlbumPhoto", id: album.id }];
+        },
         query: (album) => {
           return {
-            method: 'POST',
-            url: '/photos',
+            method: "POST",
+            url: "/photos",
             body: {
               albumId: album.id,
               url: faker.image.abstract(150, 150, true),
@@ -32,9 +42,12 @@ const photosApi = createApi({
         },
       }),
       removePhoto: builder.mutation({
+        invalidatesTags: (result, error, photo) => {
+          return [{ type: "Photo", id: photo.id }];
+        },
         query: (photo) => {
           return {
-            method: 'DELETE',
+            method: "DELETE",
             url: `/photos/${photo.id}`,
           };
         },
