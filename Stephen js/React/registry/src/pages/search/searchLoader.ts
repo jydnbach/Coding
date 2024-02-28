135769dae4c0059c606d796dea529ddcb8f5f9cf
jydnbach <jaydenkim95@gmail.com>
2024-02-28 10:43:12 +0530

    ..

--- Stephen js/React/registry/src/pages/search/searchLoader.ts
+++ Stephen js/React/registry/src/pages/search/searchLoader.ts
@@ -1,8 +1,6 @@
-
-
 import { searchPackages } from "../../api/queries/searchPackages";
 
-export async function searchLoader( {request}: {request: Request} ) => {
+export async function searchLoader({ request }: { request: Request }) {
   const { searchParams } = new URL(request.url);
   const term = searchParams.get("term");
 
@@ -10,10 +8,9 @@ export async function searchLoader( {request}: {request: Request} ) => {
     throw new Error("Search term must be provided");
   }
 
-  const results = await searchPackages(term)
+  const results = await searchPackages(term);
 
-  return{
+  return {
     searchResults: results,
   };
-};
-
+}
