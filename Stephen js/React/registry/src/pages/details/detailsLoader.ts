e9d1665ec732cefcf8ce4af7db6edf787c0de6fa
jydnbach <jaydenkim95@gmail.com>
2024-02-28 11:54:19 +0530

    details loader

--- Stephen js/React/registry/src/pages/details/detailsLoader.ts
+++ Stephen js/React/registry/src/pages/details/detailsLoader.ts
@@ -1,4 +1,40 @@
-export async function detailsLoader() {
-  console.log(`hi`);
-  return "data";
+import type { Params } from "react-router-dom";
+import { getPackage } from "../../api/queries/getPackage";
+
+interface LoaderArgs {
+  params: Params;
+}
+
+export async function detailsLoader({ params }: LoaderArgs) {
+  const { name } = params;
+
+  if (!name) {
+    throw new Error("Name must be provided");
+  }
+
+  const details = await getPackage(name);
+
+  return {
+    details,
+  };
+}
+
+
+
+import getpackeg from ../ ../ apie queris get package
+
+
+import type {Params} from reactrouter dom
+
+
+interface LoaderAergs {
+    params: Params
+}
+
+export asycn fjicotn ftailloader({params}: LoaderArgs) {
+    searchLoader
+
+    
+    
+return 'data'
 }
