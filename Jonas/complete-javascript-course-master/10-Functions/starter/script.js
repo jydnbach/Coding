376e54403583c21daeb5505675dbc9357aee3ff2
jydnbach <jaydenkim95@gmail.com>
2024-04-30 11:14:14 +0900

    function methods

--- Jonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ Jonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -2,18 +2,41 @@
 //reference point the same
 //
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+const high5 = function () {
+  console.log(`five`);
 };
 
-const upper = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
+document.body
+  .addEventListener('click', high5)
+
+  [('Jonas', 'Martha', 'Adam')].forEach(high5);
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+const lugthansa = {
+  airline: 'Lufthansa',
+  iatacode: 'LH',
+  booking: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
 };
 
-const transformer = function (str, fn) {
-  console.log(`original: ${str} / transformed: ${fn(str)}`);
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
 };
 
-transformer('javascript is the best', oneWord);
-transformer('javascript is the best', upper);
+const swiss = {
+  airline: 'swiss Airlines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(eurowings, 234, 'john');
+book.call(lufthansa, 432, 'smith');
+book.call(swiss, 623, 'lucy');
