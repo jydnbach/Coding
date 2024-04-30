b171192f8e81bb8faa3ab22f9af54c8512c87047
jydnbach <jaydenkim95@gmail.com>
2024-04-30 10:43:10 +0900

    functions

--- Jonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ Jonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+//reference point the same
+//
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upper = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`original: ${str} / transformed: ${fn(str)}`);
+};
+
+transformer('javascript is the best', oneWord);
+transformer('javascript is the best', upper);
