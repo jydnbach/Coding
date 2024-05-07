0f88adf365c464b6ec28946bd83e5310496e41f3
jydnbach <jaydenkim95@gmail.com>
2024-05-07 16:51:48 +0900

    map, filter, reduce

--- Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -82,6 +82,25 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -109,36 +128,62 @@ const currenciesUnique = new Set(['USD', 'GBP', 'USD']);
 currenciesUnique.forEach(function (value, _, map) {});
 //no keys for sets
 
-/* 
-Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+const eurToUsd = 1.1;
 
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
 
-1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
-4. Run the function for both test datasets
+const movementsDescriptions = movements.map((mov, i) => {
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+    mov
+  )}`;
+});
 
-HINT: Use tools from all lectures in this section so far 😉
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+const withdrawals = movements.filter(mov => mov < 0);
 
-TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  return acc + cur;
+}, 0);
 
-GOOD LUCK 😀
-*/
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
 
-const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuldiaCorrected = dogsJulia.slice();
-  dogsJuldiaCorrected.splice(0, 1);
-  dogsJuldiaCorrected.splice(-2);
-  const dogs = dogsJuldiaCorrected.concat(dogsKate);
+// const calcDogAge = function (dogs) {
+//   const humanAges = dogs.map(function (dogAge, i) {
+//     if (dogAge <= 2) {
+//       return 2 * dogAge;
+//     } else {
+//       return 16 + dogAge * 4;
+//     }
+//   });
 
-  dogs.forEach(function (dog, i) {
-    if (dog >= 3) {
-      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
-    } else {
-      console.log(`Dog number ${i + 1} is still a puppt.`);
-    }
-  });
+//   const above18 = humanAges.filter(function (age) {
+//     return age >= 18;
+//   });
+
+//   const sum = above18.reduce((acc, age) => {
+//     return acc + age;
+//   }, 0);
+
+//   const average = Math.trunc(sum / above18.length);
+
+//   return average;
+// };
+// TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+// TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return average;
 };
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
