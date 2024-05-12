ba2baeb200ca4fcacb72a39a672f2636010bceac
jydnbach <jaydenkim95@gmail.com>
2024-05-12 16:00:03 +0900

    event handlers

--- Jonas/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ Jonas/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,20 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1');
+
+const h1alert = e => {
+  alert('alert');
+};
+
+h1.addEventListener('mouseenter', h1alert);
+
+setTimeout(() => h1.removeEventListener('mouseenter', h1alert), 200);
