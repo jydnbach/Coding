e2891dd0de0be61669459c59edb3ff33e01b527e
jydnbach <jaydenkim95@gmail.com>
2024-05-14 13:51:22 +0900

    classes oop

--- Jonas/complete-javascript-course-master/14-OOP/starter/script.js
+++ Jonas/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,39 +1,5 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
-
-const jonas = new Person('Jonas', 1991);
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-
-console.log(jonas, matilda, jack);
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
-
-jonas.calcAge();
-
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
-
-console.log(jonas.__proto__.__proto__);
-
-console.log(Person.prototype.constructor);
-
-const arr = [3, 4, 56, 7];
-console.log(arr.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-
-console.log(arr.unique());
-
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -43,17 +9,83 @@ Car.prototype.accelerate = function () {
   this.speed += 10;
   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
+
 Car.prototype.brake = function () {
   this.speed -= 5;
   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-const bmw = new Car('BMW', 120);
-const mercy = new Car('Mercedes', 95);
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.brake();
-bmw.brake();
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('rivian', 120, 23);
+console.log(rivian);
+
+rivian.accelerate().accelerate().brake().chargeBattery(50).accelerate();
