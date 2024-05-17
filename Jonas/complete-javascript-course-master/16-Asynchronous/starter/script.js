2ea5fc52c628db1e470ffaf4658e9d8a303b95d6
jydnbach <jaydenkim95@gmail.com>
2024-05-17 10:32:06 +0900

    json

--- Jonas/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ Jonas/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -3,6 +3,38 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+                  <article class="country ${className}">
+                  <img class="country__img" src="${data.flag}" />
+                  <div class="country__data">
+                  <h3 class="country__name">${data.name}</h3>
+                  <h4 class="country__region">${data.region}</h4>
+                  <p class="country__row"><span>👫</span>${(
+                    +data.population / 1000000
+                  ).toFixed(1)}</p>
+                  <p class="country__row"><span>🗣️</span>${
+                    data.languages[0].name
+                  }</p>
+                  <p class="country__row"><span>💰</span>${
+                    data.currencies[0].name
+                  }</p>
+                  </div>
+                  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
+
+const getJSON = function (url, errorMsg = `something wnet wrong`) {
+  return fetch(url).then(res => {
+    if (!res.ok) throw new Error(`${errorMsg} (${res.status})`);
+    return res.json();
+  });
+};
+
 ///////////////////////////////////////
 
 // const getCountryData = function (country) {
@@ -73,32 +105,27 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('USA');
 // getCountryData('germany');
 
-const renderCountry = function (data, className = '') {
-  const html = `
-                <article class="country ${className}">
-                <img class="country__img" src="${data.flag}" />
-                <div class="country__data">
-                <h3 class="country__name">${data.name}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(
-                  +data.population / 1000000
-                ).toFixed(1)}</p>
-                <p class="country__row"><span>🗣️</span>${
-                  data.languages[0].name
-                }</p>
-                <p class="country__row"><span>💰</span>${
-                  data.currencies[0].name
-                }</p>
-                </div>
-                </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(res => res.json())
-    .then(data => renderCountry(data[0]));
-};
+  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
 
-getCountryData('portugal');
+      if (!neighbour) throw new Error(`No neighbor found`);
+
+      return getJSON(
+        `https://restcountries.com/v2/alpha/${neighbour}`,
+        'Country not found'
+      );
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      renderError(`something went wrong ${err.message}. try again`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
