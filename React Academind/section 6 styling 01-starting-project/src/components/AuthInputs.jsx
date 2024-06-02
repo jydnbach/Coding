3441791394bc036b3b101868ccb6ef50689bd6ac
jydnbach <jaydenkim95@gmail.com>
2024-06-02 20:12:43 +0900

    .

--- React Academind/section 6 styling 01-starting-project/src/components/AuthInputs.jsx
+++ React Academind/section 6 styling 01-starting-project/src/components/AuthInputs.jsx
@@ -1,15 +1,7 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 
 import Button from "./Button";
 import Input from "./Input";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -32,24 +24,27 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
-          $invalid={emailNotValid}
+          invalid={emailNotValid}
           type="email"
           onChange={(event) => handleInputChange("email", event.target.value)}
         />
 
         <Input
           label="Password"
-          $invalid={passwordNotValid}
+          invalid={passwordNotValid}
           type="password"
           onChange={(event) =>
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlContainer>
+      </div>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
