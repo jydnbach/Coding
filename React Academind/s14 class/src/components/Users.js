c096195c9626a149fdb9ffa03f2b5d6fed46bc40
jydnbach <jaydenkim95@gmail.com>
2024-06-09 16:17:30 +0900

    class react

---
+++ React Academind/s14 class/src/components/Users.js
@@ -0,0 +1,59 @@
+import { useState, Component } from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+class Users extends Component {
+  constructor() {
+    super();
+    //state is always object in class. name must be "state"
+    this.state = {
+      showUsers: true,
+      more: 'Test',
+    };
+  }
+
+  toggleUsersHandler() {
+    //doesn't override if not mentioned
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+const Users = () => {
+  const [showUsers, setShowUsers] = useState(true);
+
+  const toggleUsersHandler = () => {
+    setShowUsers((curState) => !curState);
+  };
+
+  const usersList = (
+    <ul>
+      {DUMMY_USERS.map((user) => (
+        <User key={user.id} name={user.name} />
+      ))}
+    </ul>
+  );
+};
+
+export default Users;
